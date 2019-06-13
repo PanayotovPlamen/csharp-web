2a592cec7b87aa81c14244c3685a78cfa21d5e5f
PanayotovPlamen <panaiotowplamen@gmail.com>
2019-06-13 21:39:22 +0300

    Added HomeController.

--- 2019-May-Season/SoftUni-Information-Services/src/Apps/Panda/Panda.Web/Program.cs
+++ 2019-May-Season/SoftUni-Information-Services/src/Apps/Panda/Panda.Web/Program.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using SIS.MvcFramework;
+using System;
+using System.Globalization;
+using System.Threading;
 
 namespace Panda.Web
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            
+            //Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            WebHost.Start(new Startup());
         }
     }
 }
---
+++ 2019-May-Season/SoftUni-Information-Services/src/Apps/Panda/Panda.Web/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Panda.Data;
+using SIS.MvcFramework;
+using SIS.MvcFramework.DependencyContainer;
+using SIS.MvcFramework.Routing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Panda.Web
+{
+    public class Startup : IMvcApplication
+    {
+        public void Configure(IServerRoutingTable serverRoutingTable)
+        {
+            using (var db = new PandaDbContext())
+            {
+                db.Database.EnsureCreated();
+            }
+        }
+
+
+        public void ConfigureServices(SIS.MvcFramework.DependencyContainer.IServiceProvider serviceProvider)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
