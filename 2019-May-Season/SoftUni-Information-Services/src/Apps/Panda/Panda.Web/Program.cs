d1777796f7019164434a2f9dd635f24c08c939b2
PanayotovPlamen <panaiotowplamen@gmail.com>
2019-06-13 20:48:30 +0300

    Database created.

--- 2019-May-Season/SoftUni-Information-Services/src/Apps/Panda/Panda.Web/Program.cs
+++ 2019-May-Season/SoftUni-Information-Services/src/Apps/Panda/Panda.Web/Program.cs
@@ -2,11 +2,11 @@
 
 namespace Panda.Web
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
